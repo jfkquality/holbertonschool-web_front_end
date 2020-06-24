5171a083dfdac155aea82b0020c73d73df67472e
jfkquality <jfkquality@gmail.com>
2020-06-24 15:14:18 -0400

    Task 2: Init commit.

---
+++ 0x0C-Javascript_advanced/2-function_me.js
@@ -0,0 +1,12 @@
+function welcomeMessage(fullname) {
+    alert('Welcome' + " " + fullname);
+}
+let guillaume = function() {
+    welcomeMessage('Guillaume');
+}
+let alex = function() {
+    welcomeMessage('Alex');
+}
+let fred = function() {
+    welcomeMessage('Fred');
+}
