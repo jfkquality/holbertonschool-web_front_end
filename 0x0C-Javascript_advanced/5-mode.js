a88c480d9f9173b83a10ad53a1591a3dd77dd3a1
jfkquality <jfkquality@gmail.com>
2020-06-30 10:41:46 -0400

    Task 5 refactor. Change function variables to const from let.

--- 0x0C-Javascript_advanced/5-mode.js
+++ 0x0C-Javascript_advanced/5-mode.js
@@ -10,9 +10,9 @@ function changeMode (size, weight, transform, background, color) {
 }
 function main () {
 
-    let spooky = changeMode(9, "bold", "uppercase", "pink", "green");
-    let darkMode = changeMode(12, "bold", "capitalize", "black", "white");
-    let screamMode = changeMode(12, "normal", "lowercase", "white", "black");
+    const spooky = changeMode(9, "bold", "uppercase", "pink", "green");
+    const darkMode = changeMode(12, "bold", "capitalize", "black", "white");
+    const screamMode = changeMode(12, "normal", "lowercase", "white", "black");
 
     let el = document.createElement('p');
     let domString = '<p>Hello Holberton!</p>';
